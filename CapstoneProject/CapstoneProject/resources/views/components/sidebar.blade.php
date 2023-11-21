6bf58c0ea102d879293487373227149b9dd303c5
AhmadRizkyFebianto <ahmadrizky13jkl@gmail.com>
2023-11-21 14:21:26 +0700

    feat(CMS-admin): crud product

---
+++ CapstoneProject/CapstoneProject/resources/views/components/sidebar.blade.php
@@ -0,0 +1,60 @@
+<!-- Sidebar Start -->
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div>
+        <div class="brand-logo d-flex align-items-center justify-content-xl-center justify-content-md-between">
+            <a href="#" class="mt-3 text-xl-center logo-img">
+                <img src="" width="180" alt="logo">
+            </a>
+            <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+                <i class="fa-solid fa-xmark fa-2xl"></i>
+            </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+            <ul id="sidebarnav">
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">Home</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="#" aria-expanded="false">
+                        <span>
+                            <i class="fa-solid fa-house-chimney"></i>
+                        </span>
+                        <span class="hide-menu">Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">Menu</span>
+                </li>
+                <li class="sidebar-item">
+                    <ul id="ui-dropdown" class="sidebar-submenu collapse">
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="#">
+                                <i class="fa-solid fa-users fa-xl" style="color: #5D87FF;"></i>
+                                <span>User management</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="#">
+                                <i class="fa-solid fa-box fa-2xl" style="color: #5D87FF;"></i>
+                                <span>Product</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="">
+                                <i class="fa-solid fa-bag-shopping fa-xl"></i>
+                                <span>View Orders</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
+<!--  Sidebar End -->
