d37106ff4043076a0ee02918a2a2a48b7fd7573d
MarcellinusAditya <marcellinusaditya26@gmail.com>
2023-11-28 17:58:20 +0700

    fix(landing-pages):fix navbar, footer and landing pages view

--- CapstoneProject/CapstoneProject/resources/views/layouts/app.blade.php
+++ CapstoneProject/CapstoneProject/resources/views/layouts/app.blade.php
@@ -3,8 +3,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <title>{{ config('app.name') }}</title>
     <!-- Scripts -->
-    <script src="{{ asset('assets/js/color-modes.js') }}"></script>
+    {{-- <script src="{{ asset('assets/js/color-modes.js') }}"></script> --}}
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
@@ -61,6 +62,13 @@
             
         }
 
+        .jumbo a button{
+            font-family: 'Montserrat', sans-serif;
+            color: white;
+            font-size: 36px;
+            font-weight: 600;
+            
+        }
         .collection{
             margin: 600px 0 300px 0;
         }
