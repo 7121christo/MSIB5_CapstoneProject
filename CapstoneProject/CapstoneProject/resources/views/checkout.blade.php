fb11b1128eb678ccebcb7b1809261241791661fd
CHRadyatma <93330711+7121christo@users.noreply.github.com>
2023-12-07 16:24:13 +0700

    Update feat(payment)

--- CapstoneProject/CapstoneProject/resources/views/checkout.blade.php
+++ CapstoneProject/CapstoneProject/resources/views/checkout.blade.php
@@ -85,11 +85,12 @@
         payButton.addEventListener('click', function () {
           // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token.
           // Also, use the embedId that you defined in the div above, here.
-        window.snap.embed('{{ $snapToken }}', {
-            embedId: 'snap-container',
+        window.snap.pay('{{ $snapToken }}', {
+            // embedId: 'snap-container',
             onSuccess: function (result) {
               /* You may add your own implementation here */
               alert("payment success!"); console.log(result);
+
             },
             onPending: function (result) {
               /* You may add your own implementation here */
@@ -113,68 +114,8 @@
 
 
 
-    {{-- <script type="text/javascript">
-        var payButton = document.getElementById('pay-button');
-        payButton.addEventListener('click', function () {
-            @if(isset($snapToken))
-                window.snap.embed('{{ $snapToken }}', {
-                    embedId: 'snap-container',
-                    onSuccess: function (result) {
-                        window.location.href = '/invoice/{{ $transaction->id }}'
-                        console.log(result);
-                    },
-                    onPending: function (result) {
-                        alert("Waiting for your payment!"); console.log(result);
-                    },
-                    onError: function (result) {
-                        alert("Payment failed!"); console.log(result);
-                    },
-                    onClose: function () {
-                        alert('You closed the popup without finishing the payment');
-                    }
-                });
-            @else
-                alert('Snap token is not set. Check your controller.');
-            @endif
-        });
-    </script> --}}
 
-    {{-- <script type="text/javascript">
-        // For example trigger on button clicked, or any time you need
-        var payButton = document.getElementById('pay-button');
-        payButton.addEventListener('click', function () {
-          // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token.
-          // Also, use the embedId that you defined in the div above, here.
-          window.snap.embed('{{ $snapToken }}', {
-            embedId: 'snap-container',
-            onSuccess: function (result) {
-              /* You may add your own implementation here */
-              //alert("payment success!"); console.log(result);
-              window.location.href = '/invoice/{{ $transactions->id }}'
-              console.log(result);
-            },
-            onPending: function (result) {
-              /* You may add your own implementation here */
-              alert("wating your payment!"); console.log(result);
-            },
-            onError: function (result) {
-              /* You may add your own implementation here */
-              alert("payment failed!"); console.log(result);
-            },
-            onClose: function () {
-              /* You may add your own implementation here */
-              alert('you closed the popup without finishing the payment');
-            }
-          });
-        });
-      </script> --}}
 
-      <?php
-      use App\Models\Transactions;
-                $userId = Auth::user()->id;
-                $add = Transactions::create(['is_paid'=>0,'user_id'=>$userId]);
-
-              ?>
 @endsection
 
 
